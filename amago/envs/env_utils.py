ea421b3d87c138fbdbf348a1388dd883ecdefd41
jakegrigsby <jcg6dn@virginia.edu>
2024-08-19 20:14:20 -0500

    add and default to vanilla e greedy

--- amago/envs/env_utils.py
+++ amago/envs/env_utils.py
@@ -226,33 +226,42 @@ class ExplorationWrapper(ABC, gym.ActionWrapper):
 
 
 @gin.configurable
-class EpsilonGreedy(ExplorationWrapper):
+class BilevelEpsilonGreedy(ExplorationWrapper):
+    """
+    Implements the bi-level epsilon greedy exploration strategy visualized in Figure 13
+    of the AMAGO paper. Exploration noise decays both over the course of training and
+    throughout each rollout. This more closely resembles the online exploration/exploitation
+    strategy of a meta-RL agent in a new environment.
+    """
+
     def __init__(
         self,
         env: gym.Env,
-        eps_start_start: float = 1.0,
-        eps_start_end: float = 0.05,
-        eps_end_start: float = 0.8,
-        eps_end_end: float = 0.01,
+        eps_start_start: float = 1.0,  # start of training, start of rollout
+        eps_start_end: float = 0.05,  # end of training, start of rollout
+        eps_end_start: float = 0.8,  # start of training, end of rollout
+        eps_end_end: float = 0.01,  # end of training, end of rollout
         steps_anneal: int = 1_000_000,
     ):
         super().__init__(env)
-
         self.eps_start_start = eps_start_start
         self.eps_start_end = eps_start_end
         self.eps_end_start = eps_end_start
         self.eps_end_end = eps_end_end
-        self.global_slope = (eps_start_start - eps_start_end) / steps_anneal
+
+        self.start_global_slope = (eps_start_start - eps_start_end) / steps_anneal
+        self.end_global_slope = (eps_end_start - eps_end_end) / steps_anneal
         self.discrete = isinstance(self.env.action_space, gym.spaces.Discrete)
         self.global_step = 0
 
     def current_eps(self, local_step: int, horizon: int):
         ep_start = max(
-            self.eps_start_start - self.global_slope * self.global_step,
+            self.eps_start_start - self.start_global_slope * self.global_step,
             self.eps_start_end,
         )
         ep_end = max(
-            self.eps_start_end - self.global_slope * self.global_step, self.eps_end_end
+            self.eps_end_start - self.end_global_slope * self.global_step,
+            self.eps_end_end,
         )
         local_progress = float(local_step) / horizon
         current = self.global_multiplier * (
@@ -279,6 +288,28 @@ class EpsilonGreedy(ExplorationWrapper):
             assert expl_action.dtype == np.float32
         self.global_step += 1
         return expl_action
+
+
+class EpsilonGreedy(BilevelEpsilonGreedy):
+    """
+    Sets the parameters of the BilevelEpsilonGreedy wrapper to be equivalent to standard epsilon-greedy.
+    """
+
+    def __init__(
+        self,
+        env: gym.Env,
+        eps_start: float = 1.0,
+        eps_end: float = 0.05,
+        steps_anneal: int = 1_000_000,
+    ):
+        super().__init__(
+            env,
+            eps_start_start=eps_start,
+            eps_start_end=eps_end,
+            eps_end_start=eps_start,
+            eps_end_end=eps_end,
+            steps_anneal=steps_anneal,
+        )
 
 
 class ReturnHistory:
