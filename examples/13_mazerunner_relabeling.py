f333292e37f5c6a33d16c227081b4731c792eebb
jakegrigsby <jcg6dn@virginia.edu>
2025-01-05 16:18:19 -0600

    cleanups for mazerunner example

--- examples/13_mazerunner_relabeling.py
+++ examples/13_mazerunner_relabeling.py
@@ -1,6 +1,28 @@
+"""
+A demonstration of the hindsight instruction relabeling techinque discussed in the AMAGO paper -
+a generalization of Hindsight Experience Replay (HER) to sequences of multiple goals.
+The ability to relabel data is another good reason to prefer off-policy RL^2 to on-policy.
+
+This example uses the "MazeRunner" environment. MazeRunner is an adapted version of Memory Maze 
+(https://arxiv.org/abs/2210.13383) that does not require the DM Lab simulator or learning from pixels.
+For more information please refer to the AMAGO Appendix C.4.
+
+There are three steps to using hindsight relabeling:
+
+1. Make the env's observations a dict with keys for the intended goal and alternative goals achieved
+   at that timestep. Goals are often subsets of the state space. In this example, observations consist of:
+   
+   `obs` : the regular observation from the maze environment (LIDAR-ish depth sensors to the walls, timer, etc.)
+   `goals` : a sequence of k goal positions to navigate to.
+   `achieved`: the agent's current position.
+
+2. Let the policy network take the observation and goals as input, but ignore the `achieved` data.
+
+3. Use the `achieved` key to relabel data with alternative goal sequences that would lead to higher returns.
+"""
+
 from argparse import ArgumentParser
 import random
-import copy
 from functools import partial
 
 import gymnasium as gym
@@ -14,14 +36,31 @@ from amago.cli_utils import *
 
 
 def add_cli(parser):
-    parser.add_argument("--maze_size", type=int, default=11)
-    parser.add_argument("--goals", type=int, default=3)
-    parser.add_argument("--time_limit", type=int, default=250)
-    parser.add_argument("--relabel", choices=["some", "all", "none"], default="some")
+    parser.add_argument(
+        "--maze_size",
+        type=int,
+        default=11,
+        help="Dimension of randomly generated mazes (n x n). n must be odd.",
+    )
+    parser.add_argument(
+        "--goals",
+        type=int,
+        default=3,
+        help="Length of the sequence of goal positions to reach during the episode.",
+    )
+    parser.add_argument(
+        "--time_limit", type=int, default=250, help="Episode time limit."
+    )
+    parser.add_argument(
+        "--relabel",
+        choices=["some", "all", "none"],
+        default="some",
+        help="`none` skips relabeling, `all` relabels every trajectory to a success. `some` creates a mixture of varying returns.",
+    )
     parser.add_argument(
         "--randomized_actions",
         action="store_true",
-        help="Randomize the directions of movement each episode (requires context-based identitifcation of current controls)",
+        help="Randomize the directions of movement each episode (requires context-based identitifcation of current controls).",
     )
     return parser
 
@@ -29,6 +68,9 @@ def add_cli(parser):
 class HindsightInstructionReplay(Relabeler):
     """
     Hindsight Experience Replay extended to "instructions" or sequences of multiple goals.
+
+    Relabelers are passed RL trajectory data before it is padded + batched and sent to the agent
+    for training.
     """
 
     def __init__(self, num_goals: int, strategy: str = "all"):
@@ -46,15 +88,13 @@ class HindsightInstructionReplay(Relabeler):
         Agent receives `obs` and `goals` as input, and we use `achieved` to relabel failed trajectories
         with new goals that lead to more reward signal.
 
-        Comments and var names are references to pseudocode in AMAGO Appendix B Alg 1 (arXiv page 20)
+        var names are references to pseudocode in AMAGO Appendix B Alg 1 (arXiv page 20)
         """
         if self.strategy == "none":
             del traj.obs["achieved"]
             return traj
 
-        og_traj = copy.deepcopy(traj)
-
-        ## LINE 1 ##
+        # 1. Find timesteps where original goals were completed
         k = self.k
         length = len(traj.rews)
         tsteps_with_goals = np.nonzero(traj.rews[:, 0])[0].tolist()
@@ -63,10 +103,20 @@ class HindsightInstructionReplay(Relabeler):
             traj.obs["goals"][t][i] for i, t in enumerate(tsteps_with_goals)
         ]
 
-        ## LINE 2 ##
+        # 2. Determine how many relabled goals we'll add
         h = k - n if self.strategy == "all" else random.randint(0, k - n)
 
-        ## LINE 4 ##
+        # it's important that this relabeler can recreate the exact reward func
+        # and terminal signals of the original env. The best way to check that is to
+        # let successful trajs (n == self.k, traj.rews.sum() == self.k) or those
+        # where we're not adding goals (h == 0) carry on through relabeling,
+        # then check that the "relabeled" verion is the same as the original.
+        # Since we've already done this we'll save the relabel time:
+        if h == 0:
+            del traj.obs["achieved"]
+            return traj
+
+        # 3. Pick h goals that were achieved as replacements for relabeling
         # in this env, every timestep has an alternative goal
         alternative_tsteps = set(range(1, length))
         # but don't sample a timestep that achieved a real goal already
@@ -75,18 +125,20 @@ class HindsightInstructionReplay(Relabeler):
         tsteps_with_alt_goals = random.sample(candidate_tsteps, k=h)
         alternatives = [g[0] for g in traj.obs["achieved"][tsteps_with_alt_goals]]
 
-        ## Line 5 ##
+        # 4. Sort the (new) "alternative" goals and completed (real) goals in the order
+        # they'd occur in the trajectory. Leave uncompleted real goals at the end
+        # (in their original order).
         combined_goals = alternatives + goals_completed
         tsteps_with_combined_goals = tsteps_with_alt_goals + tsteps_with_goals
         # sort combined_goals by tsteps
         r = [g for _, g in sorted(zip(tsteps_with_combined_goals, combined_goals))]
-        # replace original goals with ones we know we can complete
         new_goals = traj.obs["goals"][0].copy()
         for i, new_goal in enumerate(r):
             new_goals[i, :] = new_goal
 
-        ## Line 6 ##
-        # replay the trajectory as if rewards were computed with new_goals
+        # 5. Replay the trajectory as if rewards were computed with new_goals.
+        # This step requires knowledge of the reward function (like HER). In this
+        # case: binary check that "achieved" == "goals"[current_goal_idx]
         traj.obs["goals"] = np.repeat(new_goals[np.newaxis, ...], length + 1, axis=0)
         traj.rews[:] = 0.0
         # note that `rl2s` array is prev_action + prev_reward. The rew is in the 0 index.
@@ -109,7 +161,6 @@ class HindsightInstructionReplay(Relabeler):
             if active_goal_idx >= k:
                 traj.dones[t - 1] = True
                 break
-
         # enforce early termination
         del traj.obs["achieved"]
         traj.obs = {k: v[: t + 1] for k, v in traj.obs.items()}
@@ -128,17 +179,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # setup environment
+    # the AMAGO wrapper adds the relabeling info to the obs dict
     make_train_env = lambda: MazeRunnerAMAGOEnv(
         maze_dim=args.maze_size,
         num_goals=args.goals,
         time_limit=args.time_limit,
         randomized_action_space=args.randomized_actions,
     )
-    config = {
-        "amago.nets.tstep_encoders.FFTstepEncoder.d_hidden": 128,
-        # observations contain {"obs", "goals", "achieved"} but "achieved" info can be discarded after relabeling
-        "amago.nets.tstep_encoders.FFTstepEncoder.specify_obs_keys": ["obs", "goals"],
-    }
+    config = {}
     # switch sequence model
     traj_encoder_type = switch_traj_encoder(
         config,
@@ -152,12 +200,18 @@ if __name__ == "__main__":
         args.agent_type,
         # reward_multiplier=100.,
     )
-
-    # tstep_encoder_type = switch_tstep_encoder(config, arch="ff", n_layers=2, d_hidden=128, d_output=64, specify_obs_keys=["obs", "goals"]
-    tstep_encoder_type = amago.nets.tstep_encoders.FFTstepEncoder
-
+    tstep_encoder_type = switch_tstep_encoder(
+        config,
+        arch="ff",
+        n_layers=2,
+        d_hidden=128,
+        # ignore "achieved" obs dict in the policy net.
+        specify_obs_keys=["obs", "goals"],
+    )
     exploration_type = switch_exploration(
-        config, strategy="egreedy", steps_anneal=500_000
+        config,
+        strategy="egreedy",
+        steps_anneal=500_000,  # needs re-tuning; paper used an older version
     )
     use_config(config, args.configs)
 
@@ -168,9 +222,12 @@ if __name__ == "__main__":
             args,
             make_train_env=make_train_env,
             make_val_env=make_train_env,
+            # paper made a point of using maximum context length; in practice this can be shortened with similar results
             max_seq_len=args.time_limit,
+            # make sure the entire trajectory is contained in one file that will be sent to the relabeler
             traj_save_len=args.time_limit + 1,
             stagger_traj_file_lengths=False,
+            # the default Relabeler does nothing. Here we replace it with the custom one above
             relabel_type=partial(
                 HindsightInstructionReplay,
                 num_goals=args.goals,
